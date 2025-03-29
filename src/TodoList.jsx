177ed9c75a39832ee8f0385340b2c88560d870c0
Bimal Kumar H <bimalkumarh@gmail.com>
2025-03-29 15:16:04 +0530

    selectrod asd create selecor

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,29 +1,30 @@
 import { useSelector } from 'react-redux';
 import NewTodoForm from "./NewTodoForm"
 import TodoListItem from "./TodoListItem"
+import { getCompletedTodos, getIncompleteTodos, getTodosLoading } from './selectors';
 
 export default function TodoList() {
-  const todosAreLoading = useSelector(state => !state?.loading?.value?.completed);
-
-  const todos = useSelector(state => state.todos.value);
+  const todosAreLoading = useSelector(getTodosLoading);
+  const completedTodos = useSelector(getCompletedTodos);
+  const incompleteTodos = useSelector(getIncompleteTodos);
 
   return (
     <div>
       <h1>My Todos</h1>
       <NewTodoForm />
-      {todosAreLoading ? 
-        <p>Loading</p> 
+      {todosAreLoading ?
+        <p>Loading</p>
         : (
-        <>
-          <h3>Completed:</h3>
-          {todos.map((todo, index) => (
-            <TodoListItem todo={todo} key={index} />
-          ))}
-          <h3>Incomplete:</h3>
-          {todos.map((todo, index) => (
-            <TodoListItem todo={todo} key={index} />
-          ))}
-        </>
+          <>
+            <h3>Completed:</h3>
+            {completedTodos.map((todo, index) => (
+              <TodoListItem todo={todo} key={index} />
+            ))}
+            <h3>Incomplete:</h3>
+            {incompleteTodos.map((todo, index) => (
+              <TodoListItem todo={todo} key={index} />
+            ))}
+          </>
         )}
     </div>
   )
